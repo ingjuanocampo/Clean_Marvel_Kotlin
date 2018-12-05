5b953127420ba2e93b211a28f7da31fd902e7a18
juan ocampo <juan.ocampo@globant.com>
2018-12-04 22:20:05 -0500

    Improvement into pojo response

--- app/src/main/java/com/puzzlebench/clean_marvel_kotlin/data/service/response/DataBaseResponse.kt
+++ app/src/main/java/com/puzzlebench/clean_marvel_kotlin/data/service/response/DataBaseResponse.kt
@@ -2,7 +2,7 @@ package com.puzzlebench.clean_marvel_kotlin.data.service.response
 
 import com.google.gson.annotations.SerializedName
 
-class DataBaseResponse<T>(
+class DataBaseResponse(
         @SerializedName("results") val characters: List<CharacterResponse>,
         val offset: Int,
         val limit: Int,
